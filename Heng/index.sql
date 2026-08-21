dc3d400ecf5820da8a35c787d6004204ec3edfdd
akt33v <139669931+akt33v@users.noreply.github.com>
2026-08-21 13:04:38 +0800

    Add files via upload

---
+++ Heng/index.sql
@@ -0,0 +1,21 @@
+-- ==========================================
+-- INDEXES
+-- ==========================================
+
+DROP INDEX idx_orders_branch;
+DROP INDEX idx_orders_member;
+DROP INDEX idx_orderitem_item;
+DROP INDEX idx_procurement_branch_item;
+DROP INDEX idx_procurement_item;
+DROP INDEX idx_branchstock_qty;
+DROP INDEX idx_member_expiry;
+DROP INDEX idx_pointhistory_member;
+
+CREATE INDEX idx_orders_branch ON orders(branch_id);
+CREATE INDEX idx_orders_member ON orders(member_id);
+CREATE INDEX idx_orderitem_item ON order_item(item_id);
+CREATE INDEX idx_procurement_branch_item ON procurement(branch_id, item_id);
+CREATE INDEX idx_procurement_item ON procurement(item_id);
+CREATE INDEX idx_branchstock_qty ON branch_stock(stock_quantity);
+CREATE INDEX idx_member_expiry ON member(expiry_date);
+CREATE INDEX idx_pointhistory_member ON point_history(member_id);
